51cd4c4725574fb9575ec24f64d40d51e8b4d39e
philiponions <hueycoy9@gmail.com>
2023-04-19 18:07:50 -0600

    add dockerfiles

--- GamesLibrary/obj/Debug/net7.0/GamesLibrary.AssemblyInfo.cs
+++ GamesLibrary/obj/Debug/net7.0/GamesLibrary.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("291ac7b1-dfeb-4f25-9880-87a32dae9d11")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamesLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
